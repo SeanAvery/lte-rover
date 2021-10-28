487ad65994e6b950f2b5969a98d8961f716f4916
Sean Pollock <sean@Seans-MacBook-Air.local>
2021-10-28 17:57:07 -0400

    test serial msgs

--- mcu/test_serial.py
+++ mcu/test_serial.py
@@ -9,9 +9,9 @@ escape_char = b"#";
 if __name__ == "__main__":
   ser = serial.Serial("/dev/cu.usbserial-1130", 115200);
 
-  throttle = struct.pack("c I c", throttle_index, 20, escape_char)
-  # steering = struct.pack("cIc", 90)
+  throttle = struct.pack("cIc", throttle_index, 300, escape_char)
+  steering = struct.pack("cIc", steering_index, 120, escape_char)
 
   # test sending command over the wire
   while True:
-    ser.write(throttle)
+    ser.write(steering)
