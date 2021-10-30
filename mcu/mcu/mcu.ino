3bafcd1d9c2d692feb76b2cc62a1ad74c7a5d1d7
sean <seanpollock3344@gmail.com>
2021-10-30 15:07:28 -0400

    add steering/throttle msging

--- mcu/mcu/mcu.ino
+++ mcu/mcu/mcu.ino
@@ -12,7 +12,7 @@ SoftwareSerial serialPort(0, 1);
 
 // global variables
 int angle = 90;
-int throttle = 0;
+int throttle = 90;
 int minAngle = 60;
 int maxAngle = 120;
 int centerAngle = 90;
@@ -37,6 +37,7 @@ void setup() {
 
   // setup pwm pins for steering and motor control
   steeringServo.attach(9);
+  Serial.println("steering servo attatched");
   motorEsc.attach(10);
   Serial.println("motor esc attatched");
 }
@@ -44,55 +45,27 @@ void setup() {
 void loop() {
   if (serialPort.available())
   {
-    // Serial.print("available \n");
-    // Serial.write(serialPort.read());
-
     char serialMsg[34];
     
     // int throttleMsg = serialPort.parseInt();
-    serialPort.readBytesUntil(escapeIndex, serialMsg, 34);
+    size_t msgLength = serialPort.readBytesUntil(escapeIndex, serialMsg, 34);
 
     char header;
     int value;
 
-    sscanf(serialMsg, "%c%d");
+    sscanf(serialMsg, "%c%d", header, &value);
 
-    if (serialMsg[0] == throttleIndex) {
-      throttle = value;
+    if (value > 0) {
+     if (serialMsg[0] == throttleIndex) {
+        throttle = value;
+      }
+      else if (serialMsg[0] == steeringIndex)
+      {
+        angle = value;
+      }
     }
-    else if (serialMsg[0] == steeringIndex)
-    {
-      angle = value;
-    }
-
-
-    if (serialMsg[33] == escapeIndex) 
-    {
-      // angle = 90;
-    }
-    
-//    if (throttleMsg > 0)
-//    {
-//      // throttle = throttleMsg;
-//    }
-    Serial.println(throttle);
   }
   
   motorEsc.write(throttle);
   steeringServo.write(angle);
-
-//  // put your main code here, to run repeatedly:
-////  for (angle = 60; angle <= 120; angle += 1)
-////  {
-////    steeringServo.write(angle);
-////    motorEsc.write(angle);
-////    delay(15);
-////  }
-////
-////  for (angle = 120; angle >= 60; angle -= 1)
-////  {
-////    steeringServo.write(angle);
-////    motorEsc.write(angle);
-////    delay(15);
-////  }
 }
