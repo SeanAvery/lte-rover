731e5680a6153250f54cd70d4a7232e6b95a5cdd
sean <seanpollock3344@gmail.com>
2021-10-30 21:29:44 -0400

    v4l subsystem paths

--- camera/Camera.h
+++ camera/Camera.h
@@ -1,4 +1,12 @@
+#include <string>
 
+namespace params
+{
+  std::string CSID_SUBSYSTEM = "/dev/v4l-subdev3";
+  std::string CSIPHY_SUBSYSTEM = "/dev/v4l-subdev0";
+  std::string SENSOR_SUBSYSTEM = "/dev/v4l-subdev13";
+  std::string ACTUATOR_SUBSYSTEM = "v4l-subdev7";
+}
 
 class Camera
 {
