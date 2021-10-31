bf7a7911b98a3bf3c75b5d0bcf6e1b279a25b3da
sean <seanpollock3344@gmail.com>
2021-10-30 22:06:05 -0400

    opening subdevice files

--- camera/Camera.cpp
+++ camera/Camera.cpp
@@ -2,9 +2,23 @@
 
 #include <iostream>
 #include <fstream>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <fcntl.h> 
 
 int Camera::Init()
 {
   // open subdevice files
+  csi_fd = open(params::CSID_SUBSYSTEM, O_RDWR | O_NONBLOCK);
+  csiphy_fd= open(params::CSIPHY_SUBSYSTEM, O_RDWR | O_NONBLOCK);
+
+  sensor_fd= open(params::SENSOR_SUBSYSTEM, O_RDWR | O_NONBLOCK);
+
+  actuator_fd = open(params::ACTUATOR_SUBSYSTEM, O_RDWR | O_NONBLOCK);
+
+  // start camera stream
+  // csid init
+  
+  
   return 0;
 }
--- camera/Camera.h
+++ camera/Camera.h
@@ -1,16 +1,22 @@
 #include <string>
+#include <fstream>
 
 namespace params
 {
-  std::string CSID_SUBSYSTEM = "/dev/v4l-subdev3";
-  std::string CSIPHY_SUBSYSTEM = "/dev/v4l-subdev0";
-  std::string SENSOR_SUBSYSTEM = "/dev/v4l-subdev13";
-  std::string ACTUATOR_SUBSYSTEM = "v4l-subdev7";
+  char CSID_SUBSYSTEM[] = "/dev/v4l-subdev3";
+  char CSIPHY_SUBSYSTEM[] = "/dev/v4l-subdev0";
+  char SENSOR_SUBSYSTEM[] = "/dev/v4l-subdev13";
+  char ACTUATOR_SUBSYSTEM[] = "/dev/v4l-subdev7";
 }
 
 class Camera
 {
   private:
+    int csi_fd;
+    int csiphy_fd;
+    int sensor_fd;
+    int actuator_fd;
+
     int BringUp();
 
   public:
