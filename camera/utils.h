747835adbd0e9f94469f2aed0cc891eab12b728b
sean <seanpollock3344@gmail.com>
2021-10-31 23:21:52 -0400

    port over cam_ioctl handler form op

--- camera/utils.h
+++ camera/utils.h
@@ -1,3 +1,12 @@
+#include <sys/ioctl.h>
+#include <sys/cdefs.h>
+#include <sys/_types/_errno_t.h>
+#include <errno.h>
+
+// __BEGIN_DECLS
+// extern int * __error(void);
+// #define errno (*__error())
+// __END_DECLS
 
 // keep trying if x gets interrupted by a signal
 #define HANDLE_EINTR(x)                                       \
@@ -9,3 +18,10 @@
     } while (ret == -1 && errno == EINTR && try_cnt++ < 100); \
     ret;                                                      \
   })
+
+static int cam_ioctl(int fd, unsigned long int request, void *arg, const char *log_msg = nullptr) {
+  int err = HANDLE_EINTR(ioctl(fd, request, arg));
+  if (err != 0 && log_msg) {
+  }
+  return err;
+}
