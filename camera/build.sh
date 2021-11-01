f7dc2525440512e6da7cfef3ba0b18bef9b034cb
sean <seanpollock3344@gmail.com>
2021-11-01 16:31:05 -0400

    organize

--- camera/build.sh
+++ camera/build.sh
@@ -1,1 +1,1 @@
-/opt/homebrew/share/android-ndk/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android28-clang++ -static-libstdc++ main.cpp -o camera
+/opt/homebrew/share/android-ndk/toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android28-clang++ -Wmacro-redefined -Wc99-designator -static-libstdc++ camerad/main.cpp -o camera
