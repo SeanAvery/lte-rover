8fdc9e8719173637ffc6fe50355d21210ee92a4d
sean <seanpollock3344@gmail.com>
2021-11-08 11:56:19 -0500

    keyboard controller for throttle steering

--- mcu/gamepad.py
+++ mcu/gamepad.py
@@ -1,18 +1,37 @@
-import keyboard
+import serial
+import curses
+import time
+
+throttle_index = "t";
+steering_index = "s";
+escape_char = "#";
 
 steering = 90;
 throttle = 90;
 
+def format_msg(prefix, value):
+  str_msg = "{}{:05d}{}".format(prefix, value, escape_char)
+  return bytes(str_msg, 'UTF-8')
+
 if __name__ == "__main__":
+  stdscr = curses.initscr()
+  ser = serial.Serial("/dev/cu.usbserial-1130", 115200);
+  print("connected to serial port")
   while True:
-    try:
-      if keyboard.is_pressed('w'):
-        throttle += 1
-      if keyboard.is_pressed('s'):
-        throttle -= 1
-      if keyboard.is_pressed('d'):
-        steering += 1
-      if keyboard.is_pressed('a'):
-        steering -= 1
-    except ValueError as err:
-      print("error")
+    key = stdscr.getch()
+    if key == ord('w'):
+      throttle += 1
+    if key == ord('s'):
+      throttle -= 1
+    if key == ord('d'):
+      steering += 1
+    if key == ord('a'):
+      steering -= 1
+    steering_msg = format_msg(steering_index, steering)
+    throttle_msg = format_msg(throttle_index, throttle)
+    
+    time.sleep(0.1)
+    ser.write(steering_msg)
+    time.sleep(0.1)
+    ser.write(throttle_msg)
+    
