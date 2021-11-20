6e708f6e79da014a0e8fdeb2b3ed32fd8c1dc94c
sean <seanpollock3344@gmail.com>
2021-11-20 17:01:18 -0500

    rename

--- app/Gamepad.js
+++ app/Gamepad.js
@@ -1,3 +1,19 @@
+let gamepad;
+
 window.addEventListener("gamepadconnected", e => {
   console.log("gamepad connected")
+  gamepad = navigator.getGamepads()[0]
+  console.log(gamepad)
+
 })
+
+window.addEventListener("gamepaddisconnected", e=> {
+  console.log("gamepad disconnected")
+})
+
+
+const gameLoop = () => {
+  if (gamepad) {
+  }
+  
+}
