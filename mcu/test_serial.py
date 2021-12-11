e51cc3d2a53dd0899cfa23772d3816dd0f68cfff
sean <seanpollock3344@gmail.com>
2021-12-10 20:42:12 -0500

    update

--- mcu/test_serial.py
+++ mcu/test_serial.py
@@ -2,6 +2,8 @@ import serial
 import struct
 import time
 
+mcu_port = "/dev/cu.usbserial-11310"
+
 throttle_index = "t";
 steering_index = "s";
 escape_char = "#";
@@ -17,23 +19,24 @@ def format_msg(prefix, value):
   return bytes(str_msg, 'UTF-8')
 
 if __name__ == "__main__":
-  ser = serial.Serial("/dev/cu.usbserial-1130", 115200);
+  ser = serial.Serial(mcu_port, 115200);
 
-  throttle_val = 100;
+  throttle_val = -300;
   steering_val = 90;
 
   # test sending command over the wire
   while True:
     # read serial port for logs
-    log = ser.readline().decode('utf8')
-    print(log)
+    # log = ser.readline().decode('utf8')
+    # print(log)
     # update commands
     throttle_val += 10
     throttle = format_msg(throttle_index, 110)
     steering_val += 1
     steering = format_msg(steering_index, steering_val)
     time.sleep(0.1)
-    ser.write(steering)
+    # ser.write(steering)
     time.sleep(0.1)
+    print(throttle_val)
     ser.write(throttle)
 
