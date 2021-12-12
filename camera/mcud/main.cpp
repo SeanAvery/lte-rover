0d81a24640be03b05ff1852a1fb9093da312cc5e
sean <seanpollock3344@gmail.com>
2021-12-11 20:34:20 -0500

    usb connect

--- camera/mcud/main.cpp
+++ camera/mcud/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <memory>
 #include "mcu.cpp"
+
+Mcu *usb_connect()
+{
+  std::unique_ptr<Mcu> mcu;
+  try
+  {
+    mcu = std::make_unique<Mcu>();
+  }
+  catch (std::exception &e)
+  {
+    return nullptr;
+  }
+}
 
 int main()
 {
