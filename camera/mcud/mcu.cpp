d2fa2fbc15bd35daa8cbaf873528b3616307bcd9
sean <seanpollock3344@gmail.com>
2021-12-14 22:58:14 -0500

    libusb debug

--- camera/mcud/mcu.cpp
+++ camera/mcud/mcu.cpp
@@ -7,6 +7,7 @@
 
 int Mcu::init(std::string serial)
 {
+  // init libusb
   std::cout << "initializing usb connection" << std::endl;
   ssize_t num_devices;
   libusb_device **dev_list = NULL;
@@ -16,6 +17,11 @@ int Mcu::init(std::string serial)
     std::cout << "failed to init usb context" << std::endl;
     return err;
   }
+
+  // set libusb debug level
+  libusb_set_debug(ctx, 3);
+
+  // find mcu device
   num_devices = libusb_get_device_list(ctx, &dev_list);
   if (num_devices < 0)
   {
@@ -38,13 +44,14 @@ int Mcu::init(std::string serial)
       }
       unsigned char desc_serial[100] = { 0 };
       // int ret = libusb_get_string_descriptor_ascii(dev_handle, desc.iSerialNumber, desc_serial, std::size(desc_serial));
-      int ret = libusb_get_string_descriptor_ascii(dev_handle, desc.iSerialNumber, desc_serial, std::size(desc_serial));
-      if (ret < 0)
-      {
-        std::cout << "could not fetch serial number" << std::endl;
-      }
-      usb_serial = std::string((char *)desc_serial, ret).c_str();
-      std::cout << "serial number: " << usb_serial << std::endl;
+      // int ret = libusb_get_string_descriptor_ascii(dev_handle, desc.iSerialNumber, desc_serial, std::size(desc_serial));
+      // if (ret < 0)
+      // {
+      //   std::cout << "could not fetch serial number" << std::endl;
+      // }
+      // usb_serial = std::string((char *)desc_serial, ret).c_str();
+      // std::cout << "serial number: " << usb_serial << std::endl;
+      libusb_close(dev_handle);
     }
   }
   return 0;
