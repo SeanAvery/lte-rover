5318dc07ab937b35182ad4ce6e5d9766e4e8f338
sean <seanpollock3344@gmail.com>
2022-02-18 07:25:34 -0500

    random fast com test

--- mcu/test_serial.py
+++ mcu/test_serial.py
@@ -1,18 +1,17 @@
 import serial
 import struct
 import time
+import random
 
 mcu_port = "/dev/cu.usbserial-11310"
-
 throttle_index = "t";
 steering_index = "s";
 escape_char = "#";
-
 min_steering = 60;
 max_steering = 120;
-
-min_throttle = 90;
-max_throttle=300;
+min_throttle = 20;
+max_throttle= 160;
+msg_delay=0.01
 
 def format_msg(prefix, value):
   str_msg = "{}{:05d}{}".format(prefix, value, escape_char)
@@ -21,19 +20,16 @@ def format_msg(prefix, value):
 if __name__ == "__main__":
   ser = serial.Serial(mcu_port, 115200);
 
-  throttle_val = 0;
-  steering_val = 90;
-
   # test sending command over the wire
   while True:
     # read serial port for logs
     log = ser.readline().decode('utf8')
     print(log)
-
     # update commands
-    throttle = format_msg(throttle_index, 110)
-    steering = format_msg(steering_index, steering_val)
-    time.sleep(0.1)
+    throttle = format_msg(throttle_index, random.randint(min_throttle, max_throttle))
+    steering = format_msg(steering_index, random.randint(min_steering, max_steering))
+    time.sleep(0.01)
     ser.write(steering)
     time.sleep(0.1)
+    ser.write(throttle)
 
