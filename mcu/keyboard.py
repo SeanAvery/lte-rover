885a96c1d427b371df57a3a208c16eea51711c5c
sean <seanpollock3344@gmail.com>
2022-02-19 23:02:54 -0500

    update

--- mcu/keyboard.py
+++ mcu/keyboard.py
@@ -20,11 +20,12 @@ max_right = 120;
 
 def format_msg(prefix, value):
   str_msg = "{}{:05d}{}".format(prefix, value, escape_char)
+  print(str_msg)
   return bytes(str_msg, 'UTF-8')
 
 if __name__ == "__main__":
   stdscr = curses.initscr()
-  ser = serial.Serial("/dev/cu.usbserial-11310", 115200);
+  ser = serial.Serial("/dev/cu.usbserial-1120", 115200);
   print("connected to serial port")
   while True:
     key = stdscr.getch()
