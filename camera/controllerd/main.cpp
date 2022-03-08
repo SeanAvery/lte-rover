08d12cd099a8951a38d085d0e579dc144e529a7b
sean <seanpollock3344@gmail.com>
2022-03-08 07:41:45 -0500

    init socket

--- camera/controllerd/main.cpp
+++ camera/controllerd/main.cpp
@@ -2,6 +2,9 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#define UDP_PORT 900
+#define UDP_HOST "192.168.1.9"
+
 int main()
 {
   std::cout << "starting controller daemon" << std::endl;
@@ -12,4 +15,10 @@ int main()
   std::cout << "sockfd" << sockfd << std::endl;
   struct sockaddr_in serv;
   serv.sin_family = AF_INET;
+  serv.sin_addr.s_addr = htonl(INADDR_ANY);
+  int err = bind(sockfd, (const struct sockaddr *)&serv, sizeof(serv));
+  if (err != 0)
+  {
+    std::cout << "error binding socket: " << err << std::endl;
+  }
 }
