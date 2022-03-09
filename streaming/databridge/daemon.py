9edb9c809cf7816a41485748b64cd97dd8b47b3f
sean <seanpollock3344@gmail.com>
2022-03-09 14:10:41 -0500

    init udp

--- streaming/databridge/daemon.py
+++ streaming/databridge/daemon.py
@@ -1,9 +1,14 @@
+import socket
+import time
 from flask import Flask
 from flask_socketio import SocketIO
 
 # web server runs on port 5000
 app = Flask(__name__)
-socket = SocketIO(app, async_mode=None)
+
+# setup ws <-> udp socket bridge
+ws_socket = SocketIO(app, async_mode=None)
+udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 if __name__ == "__main__":
-  socket.run(app, debug=True)
+  ws_socket.run(app, debug=True)
