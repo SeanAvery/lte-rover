fe521799b5ad13d3e1d5f89904f813ead49e9407
sean <seanpollock3344@gmail.com>
2022-03-09 14:05:41 -0500

    simple websocket

--- streaming/databridge/daemon.py
+++ streaming/databridge/daemon.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
+# web server runs on port 5000
 app = Flask(__name__)
 socket = SocketIO(app, async_mode=None)
 
+if __name__ == "__main__":
+  socket.run(app, debug=True)
