ea23c07c3bfb728b3e36d7fe0562782fac837ae3
sean <seanpollock3344@gmail.com>
2022-03-08 23:08:45 -0500

    socket workin

--- camera/controllerd/main.cpp
+++ camera/controllerd/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 #define UDP_PORT 900
 #define UDP_HOST "192.168.1.9"
+#define BUF_MAX 1024
 
 int main()
 {
@@ -13,12 +15,24 @@ int main()
   std::cout << "AF_INET" << AF_INET << std::endl;
   std::cout << "SOCK_DGRAM" << SOCK_DGRAM << std::endl;
   std::cout << "sockfd" << sockfd << std::endl;
-  struct sockaddr_in serv;
+  struct sockaddr_in serv, client;
   serv.sin_family = AF_INET;
-  serv.sin_addr.s_addr = htonl(INADDR_ANY);
+  // serv.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv.sin_addr.s_addr = inet_addr(UDP_HOST);
+  serv.sin_port = htons(UDP_PORT);
   int err = bind(sockfd, (const struct sockaddr *)&serv, sizeof(serv));
   if (err != 0)
   {
     std::cout << "error binding socket: " << err << std::endl;
+  }
+  char buffer[BUF_MAX];
+  socklen_t len = sizeof(client);
+  while (true)
+  {
+    usleep(1000000);
+    // check socket for msg
+    std::cout << "checking socket" << std::endl;
+    ssize_t n = recvfrom(sockfd, (char *)buffer, BUF_MAX, MSG_WAITALL, (struct sockaddr *) &client, &len);
+    
   }
 }
--- camera/controllerd/test.py
+++ camera/controllerd/test.py
@@ -1,6 +1,18 @@
 import socket
+import time
+
+server_addr = "192.168.1.9"
+server_port = 900
+test_msg = bytes("s00110#", 'utf-8')
 
 if __name__ == "__main__":
   print("starting udp client")
   udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+  
+  while True:
+    time.sleep(1)
+    print("sending udp packet")
+    udp_socket.sendto(test_msg, (server_addr, server_port))
+
+  
   
