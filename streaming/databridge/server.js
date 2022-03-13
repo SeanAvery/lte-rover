a0858f6958805df36d0d19d4008f326aba61a243
sean <seanpollock3344@gmail.com>
2022-03-13 01:43:59 -0500

    udp client

--- streaming/databridge/server.js
+++ streaming/databridge/server.js
@@ -2,10 +2,25 @@ const express = require("express")
 const http = require("http")
 const WebSocket= require("ws")
 const path = require("path")
+const dgram = require("dgram")
 
+// setup http server
 const app = express()
 const server = http.createServer(app)
+
+// setup udp bridge
+const udpHost = "192.168.1.9"
+const udpPort = 900
+const udpClient = dgram.createSocket("udp4")
+
+// setup websocket
 const wss = new WebSocket.Server({ server })
+
+wss.on("connection", (ws) => {
+  console.log("new websocket connection")
+})
+
+// host website
 app.use(express.static("static"))
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/static/index.html"))
