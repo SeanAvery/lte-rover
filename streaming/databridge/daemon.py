977cfca40445da2f53a547d9338ffa41f395c4a5
sean <seanpollock3344@gmail.com>
2022-03-13 01:05:29 -0500

    wip flask

--- streaming/databridge/daemon.py
+++ streaming/databridge/daemon.py
@@ -1,14 +1,25 @@
 import socket
 import time
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_socketio import SocketIO
 
 # web server runs on port 5000
-app = Flask(__name__)
+app_path = "./static"
+app = Flask(__name__, static_folder=app_path)
 
 # setup ws <-> udp socket bridge
 ws_socket = SocketIO(app, async_mode=None)
 udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+# host webpage
+@app.route("/")
+def serve():
+  return send_from_directory(app.static_folder, "index.html")
+
+@ws_socket.on('connect')
+def connect():
+  print("connected to websocket")
+
+# @socket_.on('controller', namespace='/ctrls')
 if __name__ == "__main__":
   ws_socket.run(app, debug=True)
