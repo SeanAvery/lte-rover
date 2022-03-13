4af8f8ff464a3ba87a6396185ec1524b8b312073
sean <seanpollock3344@gmail.com>
2022-03-13 03:12:43 -0400

    controller parsing

--- streaming/databridge/static/Gamepad.js
+++ streaming/databridge/static/Gamepad.js
@@ -1,10 +1,6 @@
 
 let gamepad;
 
-
-const leftTrigger = 6;
-const rightTrigger = 7;
-
 // handle gamepad
 window.addEventListener("gamepadconnected", e => {
   console.log("gamepad connected")
@@ -26,18 +22,36 @@ ws.onopen = () => {
   console.log("websocket connected")
 }
 
+// controls state
+let throttle = 90
+const minThrottle = 90
+const maxThrottle = 100
+
+let steering = 90
+const minSteering = 70
+const maxSteering = 110
+
 const gameLoop = () => {
   gamepad = navigator.getGamepads()[0]
   if (gamepad) {
+    const decelerate = gamepad.buttons[6].value
+    if (decelerate > 0) {
+      console.log("decelerate: ", decelerate)
+    }
+    const accelerate = gamepad.buttons[7].value
+    if (accelerate > 0) {
+      console.log("accelerate: ", accelerate)
+    }
+
+    // steering = gamepd.axis[0] a
+    console.log('axis', gamepad.axes)
+    steering = Math.round(90 + gamepad.axes[0]*20)
+    console.log("steering: ", steering)
     for (const button in gamepad.buttons) {
       const value = gamepad.buttons[button].value
+      // console.log(value)
       if (value > 0) {
-        if (button == leftTrigger) {
-          console.log("leftTrigger", value)
-        }
-        if (button == rightTrigger) {
-          console.log("rightTrigger", value)
-        }
+        console.log("hit: ", button)
       }
     }  
   }
