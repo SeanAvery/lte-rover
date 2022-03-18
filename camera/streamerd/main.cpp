65add8b068050b216d5dbf96c41502ba2a686657
sean <seanpollock3344@gmail.com>
2022-03-18 03:30:00 -0400

    clean up

--- camera/streamerd/main.cpp
+++ camera/streamerd/main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
+#include <unistd.h>
 #include "messaging/messaging.h"
 
 // #include <gst/gst.h>
 
 int main()
 {
-
   // camera stream subscriber
   Context *context = Context::create();
   SubSocket *subscriber = SubSocket::create(context, "roadCameraState");
   subscriber->setTimeout(100);
+
+  while (true)
+  {
+    usleep(100000);
+    Message *msg = subscriber->receive();
+  }
+    
   std::cout << "yoooo" << std::endl;
 }
