25555e2ec5df02e978f684426334d8bda7d2b502
sean <seanpollock3344@gmail.com>
2022-03-28 13:32:55 -0400

    steering with joystick working

--- mcu/gamepad.py
+++ mcu/gamepad.py
@@ -1,11 +1,39 @@
 import hid
+import time
+import serial
 
+com_path = "/dev/cu.usbserial-11340"
+baudrate = 115200
+
+# connect to gamepad
 gamepad = hid.device()
 gamepad.open(1356, 2508)
-
 gamepad.set_nonblocking(True)
 
+# connect to serial
+ser = serial.Serial(com_path, baudrate)
+
+def scale_steering(value):
+  normal = (2 * ((value - 0) / (256-0))) - 1
+  return round(90 + (normal * 20))
+
+def format_msg(prefix, value):
+  str_msg = "{}{:05d}{}".format(prefix, value, "#")
+  print(str_msg)
+  return bytes(str_msg, 'UTF-8')
+
 while True:
+  time.sleep(0.001)
   report = gamepad.read(64)
+  # left/right analog left [1]
+  # up/down analog left [2]
   if report:
-    print(report)
+    # parse steering
+    steering_val = scale_steering(report[1])
+    steering_msg = format_msg("s", steering_val)
+    print(steering_msg)
+    # parse throttle
+    # send steering
+    ser.write(steering_msg)
+    # send throttle
+
