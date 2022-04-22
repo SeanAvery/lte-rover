b4a2883ee2becf1bfc4d1a106cd9e0b0502d14f1
sean <seanpollock3344@gmail.com>
2022-04-22 16:51:11 -0400

    hacky wide video stream

--- camera/streamerd/tests/raw_encode_rtp.sh
+++ camera/streamerd/tests/raw_encode_rtp.sh
@@ -1,3 +1,6 @@
 # ffmpeg -i /data/lte-rover/vid-io -vcodec libx264 -pix_fmt bgr24 -an -f rtp rtp://192.168.1.8:5004 -loglevel debug
 
-ffmpeg -f rawvideo -framerate 30 -pix_fmt bgr24 -s 386*242 -i /data/lte-rover/vid-io -an -f rtp rtp://192.168.1.8:5004 -loglevel debug
+# ffmpeg -f rawvideo -framerate 30 -pix_fmt bgr24 -s 386*242 -i /data/lte-rover/vid-io -an -f rtp rtp://192.168.1.8:5004 -loglevel debug
+
+ffmpeg -f rawvideo -framerate 30 -pix_fmt bgr24 -s 1164*874 -i /data/lte-rover/vid-io -an -f rtp rtp://192.168.1.8:5004 -loglevel debug
+
